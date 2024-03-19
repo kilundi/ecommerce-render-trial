8c20b3cf117552d6c0253c3b4d9e58500d9ba229
kilundi <muthiani42@gmail.com>
2024-03-20 00:09:18 +0300

    fixes

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from . import models
 
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    raw_id_fields =['product']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user','status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('status', 'created_at', 'first_name', 'last_name', 'email')
+    inlines = [OrderItemInline]
+
 # Register your models here.
-admin.site.register(models.Order)
+admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderItem)
