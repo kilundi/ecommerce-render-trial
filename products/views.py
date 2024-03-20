71ff1d3d9abc41f79372dcb3e6a3a68c56f3e322
kilundi <muthiani42@gmail.com>
2024-03-20 23:18:42 +0300

    final

--- products/views.py
+++ products/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Product, Review
 
 # Create your views here.
 def Product_Page(request,pk,slug):
 
     product = get_object_or_404( Product, pk=pk, slug=slug)
+    if request.method == 'POST':
+        rating = request.POST.get('rating', 3)
+        content = request.POST.get('content', '')
+
+        if content:
+            reviews = Review.objects.filter(created_by=request.user, product=product)
+
+            if reviews.count() > 0:
+                review = reviews.first()
+                review.rating = rating
+                review.content = content
+                review.save()
+            else:
+                review = Review.objects.create(
+                    product=product,
+                    rating=rating,
+                    content=content,
+                    created_by=request.user
+                )
+
+            return redirect('products:product', pk=pk, slug=slug)
     return render(request, 'products/product.html', {'products': [product]})
