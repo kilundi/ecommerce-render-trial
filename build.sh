a26d8a2e25b1092aa06c692c1a25d7edde3c664a
kilundi <muthiani42@gmail.com>
2024-03-24 14:52:00 +0300

    python3.9 to python3.10

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ source venv/Scripts/activate  # Adjust the path accordingly for Windows
 # Build the project
 echo "Building the project ..."
 
-python3.9 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements.txt
 
 echo "Making migrations ..."
-python3.9 manage.py makemigrations --noinput
+python3.10 manage.py makemigrations --noinput
 
 echo "Running migrations..."
-python3.9 manage.py migrate --noinput
+python3.10 manage.py migrate --noinput
 
 echo "Collecting Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic --noinput --clear
 
 # Deactivate the virtual environment
 echo "Deactivating venv..."
