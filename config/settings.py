4e314e3390ed61b98a38a7c8094adf4c890d4929
kilundi <muthiani42@gmail.com>
2024-03-24 14:43:27 +0300

    build.sh vercel.json

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env('DEBUG', False)
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.now.sh']
 LOGOUT_REDIRECT_URL ='/'
 LOGIN_REDIRECT_URL ='/'
 LOGIN_URL = '/login/'
