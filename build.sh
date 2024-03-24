26840cb3a228630bdeb5cf1cf3060d2ce355dac7
kilundi <muthiani42@gmail.com>
2024-03-24 13:34:06 +0300

    biuld.sh update for vercel deploying

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Activate the virtual environment
+echo "Activating venv..."
+source venv/Scripts/activate  # Adjust the path accordingly for Windows
+
 # Build the project
 echo "Building the project ..."
 
-python3.9 -m pip install -r requirements.txt
+python3.12.2 -m pip install -r requirements.txt
 
 echo "Making migrations ..."
-python3.9 manage.py makemigrations --noinput
+python3.12.2 manage.py makemigrations --noinput
 
 echo "Running migrations..."
-python3.9 manage.py migrate --noinput
-
+python3.12.2 manage.py migrate --noinput
 
 echo "Collecting Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.12.2 manage.py collectstatic --noinput --clear
+
+# Deactivate the virtual environment
+echo "Deactivating venv..."
+deactivate
