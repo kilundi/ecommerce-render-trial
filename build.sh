12a61f9ee4cd5b776cdd3201a0585028099a6472
kilundi <muthiani42@gmail.com>
2024-03-24 14:09:48 +0300

    build.sh update

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# # Activate the virtual environment
-# echo "Activating venv..."
-# source venv/Scripts/activate  # Adjust the path accordingly for Windows
+# Activate the virtual environment
+echo "Activating venv..."
+source venv/Scripts/activate  # Adjust the path accordingly for Windows
 
 # Build the project
 echo "Building the project ..."
 
-python3 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 echo "Making migrations ..."
-python3 manage.py makemigrations --noinput
+python3.12 manage.py makemigrations --noinput
 
 echo "Running migrations..."
-python3 manage.py migrate --noinput
+python3.12 manage.py migrate --noinput
 
 echo "Collecting Static..."
-python3 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
-# # Deactivate the virtual environment
-# echo "Deactivating venv..."
-# deactivate
+# Deactivate the virtual environment
+echo "Deactivating venv..."
+deactivate
